698bdaa0b8ad0f6b22feb187cc81b3504bb57fe0
MatheusDamodara <107012852+MatheusDamodara@users.noreply.github.com>
2023-12-28 16:49:09 -0300

    Update panqueca.cmd

--- panqueca.cmd
+++ panqueca.cmd
@@ -1,11 +1,4 @@
-@echo off
-color 9
-chcp 65001 > nul
-title aMathyzin LogClean 
-cd/
-echo Iniciando limpeza de logs em 5 segundos.
-timeout /t 5 /nobreak
-del *.log /a /s /q /f
-echo  https://discord.gg/vy9GuYMf
-start https://youtube.com/@amathyzin
+@echo off 
+title amathyzin
+echo panqueca
 pause
