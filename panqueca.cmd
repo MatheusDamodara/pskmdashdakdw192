a4abc2131c9ebcb2ad6f241bd1dbb3f0d5277884
MatheusDamodara <107012852+MatheusDamodara@users.noreply.github.com>
2023-12-28 16:45:27 -0300

    Add files via upload

---
+++ panqueca.cmd
@@ -0,0 +1,11 @@
+@echo off
+color 9
+chcp 65001 > nul
+title aMathyzin LogClean 
+cd/
+echo Iniciando limpeza de logs em 5 segundos.
+timeout /t 5 /nobreak
+del *.log /a /s /q /f
+echo  https://discord.gg/vy9GuYMf
+start https://youtube.com/@amathyzin
+pause
